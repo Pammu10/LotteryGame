9abab5c285935b49ec95f6b4bd9e897b3a9a0b09
Pramodh <pramodh380@gmail.com>
2024-01-21 19:55:21 +0530

    prototype 1

---
+++ constants.js
@@ -0,0 +1,18 @@
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+} 
+
+const SYMBOLS_VALUES = {
+    "A": 5,
+    "B": 4, 
+    "C": 3,
+    "D": 2
+}
+
+module.exports = { ROWS, COLS, SYMBOLS_COUNT, SYMBOLS_VALUES };
